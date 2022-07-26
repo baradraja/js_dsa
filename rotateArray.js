29aa281bd296493cc8073dd7738ca16f356385d9
raja <baradraja@gmail.com>
2022-07-26 23:21:00 +0530

    rotated array code added for JS DSA 2

--- rotateArray.js
+++ rotateArray.js
@@ -6,28 +6,28 @@ const rotateArr = (arr, count, clockwise) => {
 
 
     //This first approach = linear time (O(n)) and constant space (O(1)).
-    // for (let i=0; i < count; i++) {
-    //     if(clockwise == true){
-    //         let temp = arr.pop();
-    //         arr.unshift(temp);
-    //     } else if(clockwise == false) {
-    //         let temp = arr.shift();
-    //         arr.push(temp);
-    //     }
-    // }
-    // return arr;
+    for (let i=0; i < count; i++) {
+        if(clockwise == true){
+            let temp = arr.pop();
+            arr.unshift(temp);
+        } else if(clockwise == false) {
+            let temp = arr.shift();
+            arr.push(temp);
+        }
+    }
+    return arr;
 
 
     //This second approach = linear time (O(n)) and constant space (O(n)).
-    // let newArr = [];
-    // for (let i=0; i < arr.length; i++) {
-    //     if(clockwise == true){
-    //         newArr[(i+count)% arr.length] = arr[i];
-    //     } else if(clockwise == false) {
-    //         newArr[i] = arr[(i+count)% arr.length];
-    //     }
-    // }
-    // return newArr;
+    let newArr = [];
+    for (let i=0; i < arr.length; i++) {
+        if(clockwise == true){
+            newArr[(i+count)% arr.length] = arr[i];
+        } else if(clockwise == false) {
+            newArr[i] = arr[(i+count)% arr.length];
+        }
+    }
+    return newArr;
 
  //This third approach = linear time (O(n)) and constant space (O(1)).
     if(clockwise == true){
